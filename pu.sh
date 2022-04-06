ef4b4f3ee843bdb4ffa855cf2e362d13f16534a5
ruben <rubendasilva@me.com>
2022-04-06 16:46:34 -0700

    test commit

---
+++ pu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+git add .
+echo "<><><><><>"
+echo
+echo "Enter commit message: "
+read cmtMsg
+git commit -m "$cmtMsg"
+echo "<><><><><>"
+echo
+echo "Enter branch to push (master or main)?"
+read pshBrnch
+git push -u origin $pshBrnch
+echo "<><><>"
+echo
+echo "Push complete :) "
+echo
+echo "<><><>"
