1718a84983a9422ddf7bc527fe251ce89551c593
ruben <rubendasilva@me.com>
2022-04-06 20:56:06 -0700

    so close! couldn't get the delete button to work on appended <tr>. Delete button does work on existing <tr> within index.html

--- js/main.js
+++ js/main.js
@@ -1,30 +1,24 @@
+//Will only delete <tr> given in document (index.html)
 $('#delete').on('click', function(){
     $(this).closest('tr').fadeOut(1000, function(){
         $(this).remove();
     });
 });
-
-const newSkills = [
-    {},
-    {address: "316 Annandale Drive", sf: "1,326", bedrooms: 4, baths: 2, price: "$275,000"},
-    {address: "251 Grandview Road", sf: "3,800", bedrooms: 3, baths: 2, price: "$699,900"},
-    {address: "28571 Manitoba", sf: "2,960", bedrooms: 4, baths: 3.5, price: "$775,000"}
-];
+//nope
+$('.plsDelete').on('click', function(){
+    $(this).closest('tr').fadeOut(1000, function(){
+        $(this).remove();
+    });
+});
 
 $('#button').on('click', function(){
-    if(!newHomes.length) return;
-    const home = newHomes.pop();
+    let input = $('#field').val();
     let newRowElement = `
         <tr>
-            <td>${home.address}</td>
-            <td>${home.sf}</td>
-            <td>${home.bedrooms}</td>
-            <td>${home.baths}</td>
-            <td>${home.price}</td>
-            <td><button class="btn btn-xs btn-danger">Remove</button></td>
+            <td><button id="delete" class="plsDelete">X</button></td>
+            <td>${input}</td>
         </tr>
     `;
-    $('#homes tbody').append(newRowElement);
-    //if no more homes to add, disable button
-    if(!newHomes.length) $('#addHome').attr('disabled', true);
+    $('#appendHere').append(newRowElement);
 });
+//appends, but doesn't carry button to where I can delete
